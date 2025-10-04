e45937adff1861b9088624b901a3b02521aa90f6
ctrln3rd <markaustine001@gmail.com>
2025-10-04 16:18:20 +0300

    adding title

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home(){
   const [weatherCodeHome, seWeathercodeHome] = useState<number>(0)
   return(
      <div className={`flex flex-col items-center gap-10 pt-[10dvh] min-h-dvh pb-10 bg-fixed ${getWeatherGradientClass(weatherCodeHome)}`}>
+      <h1 className="font-semibold text-lg md:text-xl lg:text-2xl"> -- Lunara Watch --</h1>
       <Animations weatherCode={weatherCodeHome}/>
        <LocationManagerUI activeLocation={activeLocation} setActiveLocation={setActiveLocation}/>
        <Weather activeLocation={activeLocation} setWeatherCodeHome={seWeathercodeHome}/>
